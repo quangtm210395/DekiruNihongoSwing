7e3546025296b9f7cef7774107f39ff81ffc78d8
Quang <quangtmse04402@fpt.edu.vn>
2016-10-07 17:10:07 +0700

    add comment for DNObject

--- trunk/3.Project/Dekiru_Nihongo/src/com/fpt/dn/entity/DNObject.java
+++ trunk/3.Project/Dekiru_Nihongo/src/com/fpt/dn/entity/DNObject.java
@@ -13,26 +13,51 @@ public class DNObject {
     private String key;
     private String value;
 
+    /**
+     * Constructor
+     * @param key
+     * @param value
+     */
     public DNObject(String key, String value) {
         this.key = key;
         this.value = value;
     }
 
+    /**
+     * get key
+     * @return
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * set key
+     * @param key
+     */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /**
+     * get value
+     * @return
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * set value
+     * @param value
+     */
     public void setValue(String value) {
         this.value = value;
     }
     
+    /**
+     * return an object to add to table
+     * @return
+     */
     public Object[] toObject(){return new Object[]{key,value};}
 }
